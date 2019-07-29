1edabe8cf0f2313608633aee64e0d484e24cf8f9
Ayan Saha <37292741+ayansaha33@users.noreply.github.com>
2019-07-29 19:13:42 +0530

    Add files via upload

---
+++ src/main/java/com/thinkwings/nestmate/NestmateApplication.java
@@ -0,0 +1,13 @@
+package com.thinkwings.nestmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestmateApplication.class, args);
+	}
+
+}
